8b39fdf11ec2820285a86e43978ba5b343210b7a
TheCoderAS <aaloksah766626@gmail.com>
2020-06-16 23:57:41 +0530

    Updates

---
+++ precache-manifest.6234e9b0445a78668b735a0de77908b1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93cc0399c1f40337c819d9fe972fe1f2",
+    "url": "/AttMng/index.html"
+  },
+  {
+    "revision": "2f3ddc556f8be702ec27",
+    "url": "/AttMng/static/css/2.9b33be70.chunk.css"
+  },
+  {
+    "revision": "db16d5de48a52e00fdd4",
+    "url": "/AttMng/static/css/main.d56cf22b.chunk.css"
+  },
+  {
+    "revision": "2f3ddc556f8be702ec27",
+    "url": "/AttMng/static/js/2.2617b1e9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/AttMng/static/js/2.2617b1e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db16d5de48a52e00fdd4",
+    "url": "/AttMng/static/js/main.1507d92e.chunk.js"
+  },
+  {
+    "revision": "741e775e771833d47256",
+    "url": "/AttMng/static/js/runtime-main.0dd7906f.js"
+  },
+  {
+    "revision": "ba6190a930af7158effd2bb77034a174",
+    "url": "/AttMng/static/media/favicon.ba6190a9.ico"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/AttMng/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/AttMng/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/AttMng/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/AttMng/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/AttMng/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
